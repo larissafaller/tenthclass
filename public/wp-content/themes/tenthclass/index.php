da6210307310f387a7d3c8e3178c1a94b3d1c5a5
Larissa Faller <larissafaller@yahoo.com>
2014-09-25 20:00:09 +0800

    new title function

--- public/wp-content/themes/tenthclass/index.php
+++ public/wp-content/themes/tenthclass/index.php
@@ -2,9 +2,21 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Tenth Class</title>
+	<title><?php wp_title(); ?></title>
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+	
 </head>
 <body>
-	
+	<p>This is the index.php file.</p>
+	<?php 
+	if ( have_posts() ) {
+		while ( have_posts() ) {
+			the_post(); 
+			echo(' :) ');
+			the_content();
+
+	} // end while
+} // end if
+?>
 </body>
 </html>
