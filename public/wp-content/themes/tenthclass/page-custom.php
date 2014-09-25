5bdee50a0b02e83cbf46f177172a5bb87506290a
Larissa Faller <larissafaller@yahoo.com>
2014-09-25 19:58:39 +0800

    Custom page

---
+++ public/wp-content/themes/tenthclass/page-custom.php
@@ -0,0 +1,30 @@
+<?php 
+/*	
+Template Name: Your Custom Page
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php wp_title(); ?></title>
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+	
+</head>
+<body>
+	<p>We're using page-custom.php.</p>
+
+	<?php 
+
+	if ( have_posts() ) {
+		while ( have_posts() ) {
+			the_post(); 
+			echo(' :) ');
+			the_content();
+
+	} // end while
+} // end if
+?>
+</body>
+</html>
