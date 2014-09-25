d7319052a23bda7ae93fc8b915d03d15ef990290
Larissa Faller <larissafaller@yahoo.com>
2014-09-25 20:00:20 +0800

    new front page

---
+++ public/wp-content/themes/tenthclass/front-page.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php wp_title(); ?></title>
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+	
+</head>
+<body>
+	<p>We're using front-page.php.</p>
+	<?php 
+	if ( have_posts() ) {
+		while ( have_posts() ) {
+			the_post(); 
+			echo(' :) ');
+			the_content();
+
+	} // end while
+} // end if
+?>
+</body>
+</html>
