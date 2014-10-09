e925263a8ed808c6005ae03e6fdc93c42368e1e3
Larissa Faller <larissafaller@yahoo.com>
2014-10-09 20:35:23 +0800

    Added color_picker border to book of the month

--- public/wp-content/themes/tenthclass/single-books.php
+++ public/wp-content/themes/tenthclass/single-books.php
@@ -18,9 +18,10 @@
 // This is using get_field form the ACF plugin.
 // http://www.advancedcustomfields.com/resources/get_field/
 
-	$year = get_field( "year_published");
-	$author = get_field( "author_name");
-	$bookofmonth = get_field( "book_of_the_month");
+	$year = get_field( "year_published" );
+	$author = get_field( "author_name" );
+	$bookofmonth = get_field( "book_of_the_month" );
+	$bordercolor = get_field( "color_picker" );
 
 // This is the WordPress loop.
 	if ( have_posts() ) {
@@ -28,7 +29,12 @@
 			the_post(); 
 
 // If the year is greater or equal to the $year, then it will show the book details.
-			// 
+			if ($bookofmonth == true) { 
+				echo "<div class='lar_highlight' style='border: 3px solid $bordercolor;'>";
+			} else {
+				echo "<div>";
+			}
+
 			if ($year >= 1780) {
 				the_title();
 				the_content();
@@ -38,10 +44,12 @@
 					echo "This is the book of the month.";
 				}
 // If the year is less than $year, it will show this message.
-				
+
 			} else {
 				echo "Sorry, this book is too old. This book is from $year.";
-			}
+			}?>
+		</div>
+		<?php 
 
 
 
