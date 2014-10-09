2e014ba49dfc461ddde0b6a34a45ad2275c65db8
Larissa Faller <larissafaller@yahoo.com>
2014-10-09 20:07:21 +0800

    Added book of the month and colour picker.

--- public/wp-content/themes/tenthclass/single-books.php
+++ public/wp-content/themes/tenthclass/single-books.php
@@ -13,16 +13,32 @@
 	<p>We're using single-books.php.</p>
 
 	<?php
+
+
+// This is using get_field form the ACF plugin.
+// http://www.advancedcustomfields.com/resources/get_field/
+
 	$year = get_field( "year_published");
 	$author = get_field( "author_name");
+	$bookofmonth = get_field( "book_of_the_month");
 
+// This is the WordPress loop.
 	if ( have_posts() ) {
 		while ( have_posts() ) {
 			the_post(); 
 
-			if ($year >= 2000) {
+// If the year is greater or equal to the $year, then it will show the book details.
+			// 
+			if ($year >= 1780) {
 				the_title();
 				the_content();
+				echo "The author of this book is $author.</br>";
+				echo "The book was published in the year $year.</br>";
+				if ($bookofmonth == true) { 
+					echo "This is the book of the month.";
+				}
+// If the year is less than $year, it will show this message.
+				
 			} else {
 				echo "Sorry, this book is too old. This book is from $year.";
 			}
