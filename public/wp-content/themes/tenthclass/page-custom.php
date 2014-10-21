bd5695ed049e58cbf1a5de55d503063de44228ae
Larissa Faller <larissafaller@yahoo.com>
2014-10-21 20:05:00 +0800

    Sidebar SUCCESS!

--- public/wp-content/themes/tenthclass/page-custom.php
+++ public/wp-content/themes/tenthclass/page-custom.php
@@ -33,48 +33,19 @@ Template Name: Your Custom Page
 </div>	
 <?php 
 
-// get_sidebar( get_field('secondary_sidebar') );
-
-// if ( is_active_sidebar( 'main-sidebar' ) ) {
-// 	echo "<div class='sidebar'>";
-
-// 	// dynamic_sidebar puts the widget in.
-// 	dynamic_sidebar( 'main-sidebar');
-// 	echo "</div>";
-// }
-
-// if ( is_active_sidebar( 'secondary-sidebar' ) ) {
-// 	echo "<div class='sidebar'>";
-
-// 	// dynamic_sidebar puts the widget in.
-// 	dynamic_sidebar( 'secondary-sidebar');
-// 	echo "</div>";
-// }
-
-// if ( is_active_sidebar( 'tertiary-sidebar' ) ) {
-// 	echo "<div class='sidebar'>";
-
-// 	// dynamic_sidebar puts the widget in.
-// 	dynamic_sidebar( 'tertiary-sidebar');
-// 	echo "</div>";
-// }
 
 
 
 
-// Trying to connect to sidebar selector...
+// Get value for sidebar selector
 $lar_sidebar = get_field('another_sidebar_selector');
-// die($lar_sidebar);
-
-
-
 
 // If the sidebar is active (selected), then show sidebar. Else, show text.
 if ( is_active_sidebar( $lar_sidebar ) ) {
-	echo 'Sidebar is active.';
+	// echo 'Sidebar is active.';
 	echo "<div class='sidebar'>";
 	//dynamic_sidebar puts the widget in.
-	dynamic_sidebar( $lar_sidebar );
+	dynamic_sidebar( $lar_sidebar);
 	echo "</div>";
 } else {
 	echo "<div class='sidebar'>";
@@ -82,18 +53,6 @@ if ( is_active_sidebar( $lar_sidebar ) ) {
 	echo "</div>";
 }
 
-
-// 1) If every page uses the same Main Sidebar, then if you add a calendar widget to the sidebar the calendar widget will display on every page. So let's create three sidebars:
-// Main Sidebar
-// Secondary Sidebar
-// Tertiary Sidebar
-
-
-// When you create/edit a Page in WordPress, there should be a way to select which sidebar you want to appear. For example, if you create a page called, "Favourite Books Stores", you may want to have it use the Secondary Sidebar.
-
-
-
-// Finally, if you don't want to select any of the three sidebars when you create/edit a Page, there should be a textfield. You can type text and that will display instead of a sidebar. For example, on the "Favourite Books Stores" I may just want some text to display in the sidebar which says "Taiwan has the best books!".
 
 
 ?>
